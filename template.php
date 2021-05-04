40d2b55e6ba1a3422018a64cab2094e2a81e952b
Minionflo <github@minionflo.net>
2021-05-04 22:36:28 +0200

    Everything

--- template.php
+++ template.php
@@ -5,13 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rauhoff.de</title>
+    <link rel="icon" href="/img/logo.png"/>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/font/font.css">
 </head>
 <body>
     <ul>
-        <li><img src="img/logo.png"></li>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="services.php">Services</a></li>
-        <li><a href="about.php">Über uns</a></li>
+        <li id="pad"><img id="img" src="img/logo.png"></li>
+        <li><a class="activ font" href="index.html">Startseite</a></li>
+        <li><a class="font" href="services.html">Services</a></li>
+        <li><a class="font" href="about.html">Über uns</a></li>
     </ul> 
+    <h1 class="font" align=center >Rauhoff.de</h1>
 </body>
 </html>
